d755606a89b10de2b5408e4a7b2c0f3b0cd5dae7
unknown <bensandlin2@gmail.com>
2022-10-14 16:44:44 -0500

    Created tables for all ship components along with their serializers

--- backend/cars/models.py
+++ backend/cars/models.py
@@ -1,7 +1,115 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
 from authentication.models import User
 
 # Create your models here.
+
+class Tiers(models.model):
+    tier = models.IntegerField()
+    bpuLimiter = models.IntegerField()
+    special = models.IntegerField() #noting this here: this can be a single digit multiplied later on to add HP
+
+class Frames(models.Model):
+    frame = models.CharField(max_length=30)
+    size = models.CharField(max_length=12)
+    maneuverability = models.CharField(max_length=30)
+    hp = models.IntegerField()
+    increment = models.IntegerField()
+    dt = models.IntegerField()
+    ct = models.IntegerField()
+    mounts = models.CharField(max_length=150)
+    expansionBays = models.IntegerField()
+    minimumCrew = models.IntegerField()
+    maximumCrew = models.IntegerField()
+    cost = models.IntegerField()
+
+class PowerCores(models.Model):
+    core = models.CharField(max_length=30)
+    size = models.CharField(max_length=12)
+    pcu = models.IntegerField()
+    cost = models.IntegerField()
+
+class Thrusters(models.Model):
+    thurster = models.CharField(max_length=30)
+    size = models.CharField(max_length=1)
+    speed = models.IntegerField()
+    piloting = models.IntegerField()
+    pcu = models.IntegerField()
+    cost = models.IntegerField()
+
+class Armors(models.Model):
+    armor = models.CharField(max_length=30)
+    ac = models.IntegerField()
+    specialTL = models.IntegerField()
+    specialTD = models.IntegerField()
+    #cost tricky tricky needs to have math done outside of table
+
+class Computers(models.Model):
+    computer = models.CharField(max_length=30)
+    bonus = models.CharField(max_length=12)
+    nodes = models.IntegerField()
+    pcu = models.IntegerField()
+    cost = models.IntegerField()
+
+class CrewQuarters(models.Model):
+    crewQuarters = models.CharField(max_length=10)
+    cost = models.IntegerField()
+
+class DefensiveCountermeasures(models.Model):
+    defensiveCountermeasures = models.CharField(max_length=15)
+    TLbonus = models.IntegerField()
+    pcu = models.IntegerField()
+    cost = models.IntegerField()
+
+class DriftEngines(models.Model):
+    driftEngine = models.CharField(max_length=20)
+    rating = models.IntegerField()
+    pcuRequirement = models.IntegerField()
+    maxSize = models.CharField(max_length=12)
+    #cost = ah lovely, same as armor. hrm
+
+class ExpansionBays(models.Model):
+    expansionBay = models.CharField(max_length=50)
+    pcu = models.IntegerField()
+    cost = models.IntegerField()
+
+class Security(models.Model):
+    security = models.CharField(max_length=60)
+    cost = models.IntegerField()
+
+class Sensors(models.Model):
+    sensors = models.CharField(max_length=36)
+    range = models.CharField(max_length=12)
+    modifier = models.IntegerField()
+    cost = models.IntegerField()
+
+class Shields(models.Model):
+    shield = models.CharField(max_length=24)
+    totalSP = models.IntegerField()
+    regen = models.CharField(max_length=12)
+    pcu = models.IntegerField()
+    cost = models.IntegerField()
+
+class Weapons(models.Model):
+    '''
+    create a model (like user) to represent the weapon class (light, 
+    heavy, capital, with two classes in each: direct and tracking)
+    also realized that the speed field would only be applied to
+    tracking weapons, so there's that
+    actually, really, making it a field should work
+    the web app can just check to make sure it's the right class, right?
+    the type (direct or tracking) can just be another field, no need to 
+    check it
+    '''
+    weapon = models.CharField(max_length=48)
+    weaponClass = models.CharField(max_length=12) #light, heavy, capital
+    weaponType = models.CharField(max_length=12) #direct, tracking
+    speed = models.IntegerField() #only applies to tracking weapons
+    range = models.CharField(max_length=12)
+    damage = models.CharField(max_length=12)
+    pcu = models.IntegerField()
+    cost = models.IntegerField()
+    special = models.CharField(max_length=36)
 
 # <<<<<<<<<<<<<<<<< EXAMPLE FOR STARTER CODE USE <<<<<<<<<<<<<<<<<
 
--- backend/cars/serializers.py
+++ backend/cars/serializers.py
@@ -1,5 +1,90 @@
+from jmespath import search
 from rest_framework import serializers
 from .models import Car
+from .models import Tiers, Frames, PowerCores, Thrusters, Armors, Computers, CrewQuarters, DefensiveCountermeasures, DriftEngines, ExpansionBays, Security, Sensors, Shields, Weapons
+
+class TiersSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tiers
+        fields = ['tier', 'bpulimiter', 'special']
+        depth = 1
+
+class FramesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Frames
+        fields = ['frame','size','maneuverability','hp','increment','dt','ct','mounts','expansionBays','minimumCrew','maximumCrew','cost']
+        depth = 1
+
+class PowerCoresSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PowerCores
+        fields = ['core','size','pcu','cost']
+        depth = 1
+
+class ThrustersSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Thrusters
+        fields = ['thruster','size','speed','piloting','pcu','cost']
+        depth = 1
+
+class ArmorsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Armors
+        fields = ['armor','ac','specialTL','specialTD']
+        #MISSING COST FIELD! NOT READY YET! DON'T FORGET THIS! (you're going to)
+        depth = 1
+
+class ComputersSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Computers
+        fields = ['computer','bonus','nodes','pcu','cost']
+        depth = 1
+
+class CrewQuartersSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CrewQuarters
+        fields = ['crewQuarters','cost']
+        depth = 1
+
+class DriftEnginesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DriftEngines
+        fields = ['driftEngine','rating','pcuRequirement','maxSize']
+        #MISSING COST FIELD! NOT READY YET! DON'T FORGET THIS! (you're going to)
+        depth = 1
+
+class ExpansionBaysSeralizer(serializers.ModelSerializer):
+    class Meta:
+        model = ExpansionBays
+        fields = ['expansionBay','pcu','cost']
+        depth = 1
+    
+class SecuritySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Security
+        fields = ['security','cost']
+        depth = 1
+
+class SensorsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Sensors
+        fields = ['sensors','range','modifier','cost']
+        depth = 1
+
+class ShieldsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Shields
+        fields = ['shield','totalSP','regen','pcu','cost']
+        depth = 1
+
+class WeaponsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Weapons
+        fields = ['weapon','weaponClass','WeaponType','speed','range','damage','pcu','cost','special']
+
+
+
+
 
 # <<<<<<<<<<<<<<<<< EXAMPLE FOR STARTER CODE USE <<<<<<<<<<<<<<<<<
 
