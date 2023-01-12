54030348b4bc9eba8b4f3a1ee5e28b9d449a6326
unknown <bensandlin2@gmail.com>
2023-01-12 16:30:37 -0600

    Removed the login page entirely, project is back up and working again

--- frontend/public/users.js
+++ frontend/public/users.js
@@ -1,22 +1,22 @@
-let variable = `
-    <div class='container'>
-    <br />
-        <div class='input'>
-            <br />
-        <form id="login" onsubmit='LoginInput()'>
-            <label for="name">user name:</label>
-            <input type="text" id="username" name="name" value="Name" /><br />
-            <input type="password" id="password" name="password" value="password" />
-            <label for="password"></label>
-            <button type="submit">Register</button>
-            <button type="submit">Sign in</button>
-        </form>
-    </div>
-    </div>
-`
-document.getElementById("root").insertAdjacentHTML("afterbegin", variable);
+// let variable = `
+//     <div class='container'>
+//     <br />
+//         <div class='input'>
+//             <br />
+//         <form id="login" onsubmit='LoginInput()'>
+//             <label for="name">user name:</label>
+//             <input type="text" id="username" name="name" value="Name" /><br />
+//             <input type="password" id="password" name="password" value="password" />
+//             <label for="password"></label>
+//             <button type="submit">Register</button>
+//             <button type="submit">Sign in</button>
+//         </form>
+//     </div>
+//     </div>
+// `
+// document.getElementById("root").insertAdjacentHTML("afterbegin", variable);
 
-function LoginInput() {
-    user.username = document.getElementById('username').value
-    SaveToStorage();
-}
+// function LoginInput() {
+//     user.username = document.getElementById('username').value
+//     SaveToStorage();
+// }
